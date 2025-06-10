d48ad6b4b8fd7c4fd669e74f2266e9f908a78104
gugonzalez1409 <gustavo-gonzalez1409@hotmail.com>
2025-06-10 03:18:22 -0400

    fix reward

--- models/utils/reward.py
+++ models/utils/reward.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 
 class customReward(gym.Wrapper):
@@ -12,13 +13,12 @@ class customReward(gym.Wrapper):
         self.prev_x_pos = 0
         # contador de tiempo estancado
         self.stuck_time = 0
-
     def reset(self, **kwargs):
 
         # reiniciar variables
         self.score = 0
         self.status = 'small'
-        self.prev_x_pos = 0
+        self.prev_x_pos = None
         self.stuck_time = 0
 
         return self.env.reset(**kwargs)
@@ -26,14 +26,14 @@ class customReward(gym.Wrapper):
     def step(self, action):
 
         state, reward, done, info = self.env.step(action)
-        reward += (info['score'] - self.score) / 10 # recompensar la diferencia positiva de puntaje
+        reward += (info['score'] - self.score) / 20 # recompensar la diferencia positiva de puntaje
         self.score = info['score'] # asignar este puntaje como el actual
 
         
         curr_x = info['x_pos'] # posicion actual
 
         # iniciar posicion en el primer step
-        if self.prev_x_pos == 0:
+        if self.prev_x_pos is None:
             self.prev_x_pos = curr_x
 
         # en caso de retroceder, penalización mayor, no acumulativa
@@ -43,29 +43,29 @@ class customReward(gym.Wrapper):
         # en caso de estar estancado, penalizacion acumulativa
         elif curr_x == self.prev_x_pos:
             self.stuck_time += 1
-            reward -= 0.1 * self.stuck_time
+            reward -= min(0.1 * self.stuck_time, 3.0)
 
-        # en caso de avanzar, premiar, en caso de avanzar luego de
+        # en caso de avanzar, de forma significativa premiar, en caso de avanzar luego de
         # muchos steps estancado, premiar mas
         else:
-            if self.stuck_time > 30:
-                reward += 2
+            if self.stuck_time > 30 and (curr_x - self.prev_x_pos) > 3:
+                reward += min(0.5 * self.stuck_time, 5.0)
                 
             # reiniciar contador de estancado
             self.stuck_time = 0
 
         # castigar perder el powerup
         if(self.status in ['tall', 'fireball'] and info['status'] == 'small'):
-            reward -= 2
+            reward -= 2.0
 
         self.status = info['status']
 
         # premiar completar nivel
         if done:
             if info['flag_get']:
-                reward += 300
+                reward += 30.0
 
         self.prev_x_pos = curr_x
 
-        return state, reward / 10, done, info
+        return state, reward / 10.0, done, info
     
