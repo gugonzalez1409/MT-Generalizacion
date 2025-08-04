5c10d9262494c5203b3daa66be9c19cae88ee3e3
gugonzalez1409 <gustavo-gonzalez1409@hotmail.com>
2025-08-04 02:57:54 -0400

    hiper parametros de RPPO

--- models/utils/trainers.py
+++ models/utils/trainers.py
@@ -169,14 +169,14 @@ def trainRecurrentPPO(explore, random, custom, vectorized, impala, icm):
         learning_rate=linear_schedule(1.75e-4 if impala else 1e-4),
         env = vectorizedEnv(explore, random, custom, icm, recurrent) if vectorized else make_single_env(explore, random, custom),
         policy_kwargs=policy_kwargs,
-        n_steps=512,
-        batch_size=256,
+        n_steps=2048,
+        batch_size=512,
         clip_range=0.2,
         ent_coef=0.01,
         gamma=0.99,
         gae_lambda=0.95,
         verbose=1,
-        n_epochs=3,
+        n_epochs=4,
         max_grad_norm=0.5,
         tensorboard_log = tensorboard_log
     )
